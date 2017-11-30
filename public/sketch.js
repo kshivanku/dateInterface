3d5c9d878066010d764207cd0834e21742550538
kshivanku <kshivanku@gmail.com>
2017-11-30 12:21:42 -0500

    directing messages to different socket pages

--- public/sketch.js
+++ public/sketch.js
@@ -3,38 +3,41 @@ socketDates = io.connect("https://dateinterface.herokuapp.com/");
 
 var profilename;
 
-$(document).ready(function(){
-  loadpage("profileSelection");
-  $("#writer_profile_box").click(function(){
-    profilename = "writer";
-    loadpage('scriptPage');
-    $("#scriptPage h1").html("Date page for: <span style='font-weight:bold'>" + profilename + "</span>");
-    console.log("profilename: " + profilename);
-  });
-  $("#date_profile_box").click(function(){
-    profilename = "date";
-    $("#scriptPage h1").html("Date page for: <span style='font-weight:bold'>" + profilename + "</span>");
-    loadpage('scriptPage');
-    console.log("profilename: " + profilename);
-  });
+$(document).ready(function() {
+    loadpage("profileSelection");
+    $("#writer_profile_box").click(function() {
+        profilename = "writer";
+        loadpage('scriptPage');
+        $("#scriptPage h1").html("Date page for: <span style='font-weight:bold'>" + profilename + "</span>");
+        console.log("profilename: " + profilename);
+    });
+    $("#date_profile_box").click(function() {
+        profilename = "date";
+        $("#scriptPage h1").html("Date page for: <span style='font-weight:bold'>" + profilename + "</span>");
+        loadpage('scriptPage');
+        console.log("profilename: " + profilename);
+    });
 });
 
 socketDates.on('scriptCues', gotScriptCues);
 function gotScriptCues(data) {
-  if (data.intendedFor.indexOf(profilename) != -1) {
-
-  }
+    if (data.intendedFor.indexOf(profilename) != -1) {
+        $("#topics").empty();
+        var topic = data.message.split(": ")[1];
+        var topicDiv = `<h2>Talk about ` + topic + `</h2>`;
+        $("#topics").append(topicDiv);
+    }
 }
 
 function loadpage(page) {
-  switch(page){
-    case 'profileSelection':
-      $("#profileSelectionPage").css('display', 'block');
-      $("#scriptPage").css('display', 'none');
-      break;
-    case 'scriptPage':
-      $("#profileSelectionPage").css('display', 'none');
-      $("#scriptPage").css('display', 'block');
-      break;
-  }
+    switch (page) {
+        case 'profileSelection':
+            $("#profileSelectionPage").css('display', 'block');
+            $("#scriptPage").css('display', 'none');
+            break;
+        case 'scriptPage':
+            $("#profileSelectionPage").css('display', 'none');
+            $("#scriptPage").css('display', 'block');
+            break;
+    }
 }
