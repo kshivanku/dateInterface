ed44ee407f1cddb70d804313f5c2f5d588e57e19
kshivanku <kshivanku@gmail.com>
2017-11-30 12:02:00 -0500

    making sockets work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Web Server
 var express = require('express');
 var app = express();
-var server = app.listen(process.env.PORT || 8080, function(){
+var server = app.listen(process.env.PORT || 8000, function(){
   console.log('serverstarted');
 })
 app.use(express.static("public"));
@@ -11,5 +11,10 @@ var socket = require('socket.io');
 var io = socket(server);
 
 io.sockets.on('connection', function(socket) {
-  console.log('connected: ' + socket.id);
+  console.log('dates socket connected to some client: ', socket.id);
+
+  socket.on('scriptCues', function(data) {
+      console.log("got script cues");
+      socket.broadcast.emit('scriptCues', data);
+  });
 });
