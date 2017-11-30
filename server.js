d1cb17f8003d897a9031db28863face31d65623e
kshivanku <kshivanku@gmail.com>
2017-11-30 03:59:29 -0500

    making a socket server

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ var server = app.listen(process.env.PORT || 8080, function(){
   console.log('serverstarted');
 })
 app.use(express.static("public"));
+
+//Socket stuff
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', function(socket) {
+  console.log('connected: ' + socket.id);
+}
