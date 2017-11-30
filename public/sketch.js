a051f48608f7021a21310fa05ddc0efdcff41bb4
kshivanku <kshivanku@gmail.com>
2017-11-30 04:53:16 -0500

    added a profile page

--- public/sketch.js
+++ public/sketch.js
@@ -1,55 +1,38 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDB7c6Uf8dcA5m8jrWEyjKSPK7crGENyyQ",
-  authDomain: "audiencepoll-7e23c.firebaseapp.com",
-  databaseURL: "https://audiencepoll-7e23c.firebaseio.com",
-  projectId: "audiencepoll-7e23c",
-  storageBucket: "audiencepoll-7e23c.appspot.com",
-  messagingSenderId: "580263770976",
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+var socketDates;
+socketDates = io.connect("https://dateinterface.herokuapp.com/");
 
-var ref = database.ref("allusers");
-ref.on('value', gotData, errData);
+var profilename;
 
-var count = 1;
-function gotData(data){
-  console.log(data.val())
-  var users = Object.keys(data);
-  if(data.val() != null && count == 1){
-    $("h1").html("Your date is going: Terrible");
-    $("body").css("background-color", "#ff0000");
-    $("body").css("color", "#ffffff");
-    count++;
-  }
-  else if(data.val() != null && count == 2){
-    $("h1").html("Save this date by telling a personal story!");
-    $("body").css("background-color", "#ffff00");
-    $("body").css("color", "#212121");
-    count = 1;
+$(document).ready(function(){
+  loadpage("profileSelection");
+  $("#writer_profile_box").click(function(){
+    profilename = "writer";
+    loadpage('scriptPage');
+    console.log("profilename: " + profilename);
+  });
+  $("#date_profile_box").click(function(){
+    profilename = "date";
+    loadpage('scriptPage');
+    console.log("profilename: " + profilename);
+  });
+});
+
+socket.on('scriptCues', gotScriptCues);
+function gotScriptCues(data) {
+  if (data.intendedFor == profilename) {
+    console.log("do something");
   }
 }
 
-function errData(err){
-  console.log(err);
-}
-
-
-var questions = [
-  {
-    "question_id": "Both01",
-    "question_text": "How is the date going so far?",
-    "pop_answer": null
-  },
-  {
-    "question_id": "HIM01",
-    "question_text": "Quick, save the conversation! What should he talk about next?",
-    "pop_answer": null
-  },
-  {
-    "question_id": "HER01",
-    "question_text": "What do you think is the most important quality that he is looking for in his partner?",
-    "pop_answer": null
+function loadpage(page) {
+  switch(page){
+    case 'profileSelection':
+      $("#profileSelectionPage").css('display', 'block');
+      $("#scriptPage").css('display', 'none');
+      break;
+    case 'scriptPage':
+      $("#profileSelectionPage").css('display', 'none');
+      $("#scriptPage").css('display', 'block');
+      break;
   }
-]
+}
